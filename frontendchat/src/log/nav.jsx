9f00692101c000b4baabad12ac510de79323aa71
MoatafaRabie <mostaga.kxkk34@gmail.com>
2026-02-09 23:39:01 +0200

    Update nav.jsx

--- frontendchat/src/log/nav.jsx
+++ frontendchat/src/log/nav.jsx
@@ -53,7 +53,7 @@ const Nav = () => {
               </svg>
               <span>Chat</span>
             </NavLink>
-            <span className="text-gray-300 text-sm hidden sm:inline">| Welcome, {authUser.user.username}</span>
+            <span className="text-gray-300 text-sm ">| Welcome, {authUser.user.username}</span>
             <button
               className="bg-red-600 text-white rounded-md p-2 px-5 hover:bg-red-500 transition font-bold text-sm"
               onClick={handleLogout}
@@ -86,3 +86,4 @@ const Nav = () => {
 
 
 export default Nav;
+
