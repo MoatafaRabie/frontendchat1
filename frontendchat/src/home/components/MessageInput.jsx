1962e752d5d68aa2e26276a72069672677106d86
Mostafa Moahmed Rabie Eid Abdelhamid <mostaga.kxkk34@gmail.com>
2026-05-11 19:03:51 +0300

    Change message sending API to secure endpoint
    Updated the API endpoint for sending messages to a secure server.

--- frontendchat/src/home/components/MessageInput.jsx
+++ frontendchat/src/home/components/MessageInput.jsx
@@ -17,7 +17,7 @@ const MessageInput = () => {
         setLoading(true);
         try {
             const res = await axios.post(
-                `http://localhost:3001/api/message/send/${selectedConversation._id}`, 
+                `https://vulnerable-abagail-personalllllll-3a6b55d5.koyeb.app/api/message/send/${selectedConversation._id}`, 
                 { messages: messageText }, 
                 { withCredentials: true }
             );
